3ce2a2a8ce47da1d067522b2bd25e890f6245ecf
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-26 02:39:59 +0300

    Made the inside modal

--- movie-app/src/MovieBox.js
+++ movie-app/src/MovieBox.js
@@ -1,15 +1,41 @@
+import { Modal } from "react-bootstrap";
 import React from "react";
 
-const API_IMG="https://image.tmdb.org/t/p/w500/";
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
 
-const MovieBox = ({title, poster_path, vote_average, release_date, overview})=> {
-    return (
-        <div>
-            <h1>{title}</h1>
-            <img src={API_IMG+poster_path} alt="img"></img>
-            <p>{overview}</p>
+const MovieBox = ({
+  title,
+  poster_path,
+  vote_average,
+  release_date,
+  overview,
+}) => {
+  return (
+    <div className="card text-center bg-secondary mb-3">
+      <div className="card-body">
+        <img className="card-img-top" src={API_IMG + poster_path} alt="img" />
+        <div className="card-body">
+          <button type="button" className="btn btn-dark">
+            Preview
+          </button>
+          <Modal>
+            <Modal.Header closeButton>
+              <Modal.Title></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img className="card-img-top" src={API_IMG + poster_path} alt="img"></img>
+              <h3>{title}</h3>
+              <h4>ImDb: {vote_average}</h4>
+              <h5>Release Date: {release_date}</h5>
+              <br></br>
+              <h6>Overview:</h6>
+              <p>{overview}</p>
+            </Modal.Body>
+          </Modal>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default MovieBox;
