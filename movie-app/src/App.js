92ec827ac3d50b0163430d214602daa2962b1bc5
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-26 03:48:39 +0300

    Started working on the navbar component

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,29 +1,45 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import MovieBox from './MovieBox';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import MovieBox from "./MovieBox";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav} from "react-bootstrap";
 
-const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=089b617253e47ebe55e8aeef2a65814f";
+const API_URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=089b617253e47ebe55e8aeef2a65814f";
 function App() {
+  const [movies, setMovies] = useState([]);
 
-  const [movies, setMovies]=useState([]);
- 
   useEffect(() => {
     fetch(API_URL)
-    .then((res)=>res.json())
-    .then(data=>{
-      console.log(data);
-      setMovies(data.results)
-    })
-  }, [])
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
 
   return (
-    <div className='container'>
-      <div className='grid'>
-      {movies.map((movieReq)=>
-      <MovieBox key={movieReq.id} {...movieReq}/>)}
+    <>
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="">MovieDb App</Navbar.Brand>
+        <Navbar.Brand href="">Trending</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll">
+
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto my-2 my-lg-3" style={{maxHeight: '100px'}} navbarScroll></Nav>
+          </Navbar.Collapse>
+        </Navbar.Toggle>
+      </Container>
+    </Navbar>
+      <div className="container">
+        <div className="grid">
+          {movies.map((movieReq) => (
+            <MovieBox key={movieReq.id} {...movieReq} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
