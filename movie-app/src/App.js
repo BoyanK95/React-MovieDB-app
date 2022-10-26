4676f04a520f9415227effdf36bc24592749ae6c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-26 18:59:04 +0300

    Made the search bar functional

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -6,8 +6,11 @@ import { Navbar, Container, Nav, Form, FormControl, Button} from "react-bootstra
 
 const API_URL =
   "https://api.themoviedb.org/3/movie/popular?api_key=089b617253e47ebe55e8aeef2a65814f";
+
+  const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=089b617253e47ebe55e8aeef2a65814f&query"
 function App() {
   const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     fetch(API_URL)
@@ -18,6 +21,24 @@ function App() {
       });
   }, []);
 
+  const searchMovie = async(e)=>{
+    e.preventDefault()
+    console.log('Searching');
+    try{
+      const url=`https://api.themoviedb.org/3/search/movie?api_key=089b617253e47ebe55e8aeef2a65814f&query=${query}`
+      const res=await fetch(url)
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  const changeHandler=(e)=>{
+    setQuery(e.target.value)
+  }
   return (
     <>
     <Navbar bg="dark" expand="lg" variant="dark">
@@ -29,13 +50,14 @@ function App() {
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-3" style={{maxHeight: '100px'}} navbarScroll></Nav>
 
-            <Form className="d-flex">
+            <Form className="d-flex" onSubmit={searchMovie}>
               <FormControl
               type="search"
               placeholder="Movie Search"
               className="me-2"
               aria-label="search"
-              name=""></FormControl>
+              name=""
+              value={query} onChange={changeHandler}></FormControl>
               <Button variant="secondary" type="submit">Search</Button>
             </Form>
           </Navbar.Collapse>
