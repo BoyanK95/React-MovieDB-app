61ad741cbb489aaab547bd4d2404b7f9c5be415e
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-26 04:39:41 +0300

    made the home link and changed the background

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -22,8 +22,8 @@ function App() {
     <>
     <Navbar bg="dark" expand="lg" variant="dark">
       <Container fluid>
-        <Navbar.Brand href="">MovieDb App</Navbar.Brand>
-        <Navbar.Brand href="">Trending</Navbar.Brand>
+        <Navbar.Brand href="/home">MovieDb App</Navbar.Brand>
+        <Navbar.Brand href="/home">Trending</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
 
           <Navbar.Collapse id="navbarScroll">
